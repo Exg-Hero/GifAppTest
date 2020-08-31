2d6eb86f0ee136bbad7c9f9c1006527165bda90e
Raul Monroy <sBlink@outlook.com>
2020-08-31 14:36:24 -0500

    docs agregados

---
+++ docs/precache-manifest.34c9488315596bbcb44069bfeb6f9678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1271be38a3d6041dd75702d4492a0b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7e92b867158cfa8c9c",
+    "url": "/static/css/main.91d5891e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7e92b867158cfa8c9c",
+    "url": "/static/js/main.7f766744.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
